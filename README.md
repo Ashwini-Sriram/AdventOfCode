71d9fba26b133f70b0d2a96d15ab842d9210a697
Ashwini Sriram <ashwinisriram@Ashwinis-MBP.Excitel>
2023-08-01 17:17:47 +0530

    Status code 200: get_input() working

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # AdventOfCode
+
+Hello welcome to play Advent of Code!
+
+To get started fork this repository to your local 
+add the AOC_TOKEN in a .env file
--- api.py
+++ api.py
@@ -1,8 +1,27 @@
-import requests 
+import requests
+import sys
+import os
+from dotenv import load_dotenv
 
-def get_input():
-    response = requests.get("https://adventofcode.com/2022/day/1/input")
-    print(response)
+load_dotenv()
+ 
+#AOC_TOKEN = '53616c7465645f5f400b0e919ccbfd43c47eb526dc1561615804bdf6248d6c21cc3b1566b0f4a4b1409a2308e036a43c1e4a8b8270df40b09a9201ba4805fd8c'
+sessionToken = os.getenv('AOC_TOKEN')
+
+print(sessionToken)
+ 
+def get_input(day):
+    url = "https://adventofcode.com/2022/day/"+str(day)+"/input"
+    headers = {'Cookie': f"session={sessionToken}"}
+    r = requests.get(url, headers=headers)
+    print(r.status_code)
+    if r.status_code == 200:
+        return r.text 
+    else:
+        sys.exit(f"/api/alerts response: {r.status_code}: {r.reason} \n{r.content}")
     
-get_input()
+
+
+    
+get_input(1)
 

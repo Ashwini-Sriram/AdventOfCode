c66ca35e73e27d6a6ec9d6e9146977bc1a563347
prachi-goenka <prachigoenka688@gmail.com>
2023-08-01 18:56:13 +0530

    getting the input file

--- api.py
+++ api.py
@@ -7,17 +7,16 @@ load_dotenv()
 
 sessionToken = os.getenv('AOC_TOKEN')
 
-print(sessionToken)
-
 def get_input(day):
     url = "https://adventofcode.com/2022/day/"+str(day)+"/input"
     headers = {'Cookie': f"session={sessionToken}"}
-    r = requests.get(url, headers=headers)
-    print(r.status_code)
-    if r.status_code == 200:
-        return r.text
+    response = requests.get(url, headers=headers)
+    print(response.status_code)
+    if response.status_code == 200:
+        print(response.content)
+        return response.text
     else:
-        sys.exit(f"/api/alerts response: {r.status_code}: {r.reason} \n{r.content}")
+        sys.exit(f"/api/alerts response: {response.status_code}: {response.reason} \n{response.content}")
 
 
 

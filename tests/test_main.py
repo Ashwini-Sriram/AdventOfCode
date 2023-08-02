564726203b63ee038b92159b5bd486d8fed28ed7
Ashwini Sriram <ashwinisriram@Ashwinis-MBP.Excitel>
2023-08-02 10:51:51 +0530

    added test template for call_solution method

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,9 @@
 import os
 
 from tests import BaseTestCase
-from api import get_input
+from api import *
+from main import *
+from solution import *
 
 cwd = os.getcwd()
 # cwd = os.path.normpath(os.getcwd() + os.sep + os.pardir)
@@ -15,6 +17,14 @@ class TestService(BaseTestCase):
         FILE_PATH =  cwd+f"/input{day}.txt"
         assert os.path.exists(FILE_PATH)
 
-
-
+    def test_call_solution():
+        with patch("main.fetch_input") as mock_fetch_input:
+            call_solution(day=1)
+        with patch("solution.get_puzzle_answer") as mock_get_puzzle_answer:
+            call_solution(day=1)
+        with patch("api.check") as mock_check_solution:
+            call_solution(day=1)
+        mock_fetch_input.assert_called_once_with(day=1)
+        mock_get_puzzle_answer.assert_called_once_with(day=1)
+        mock_check_solution.assert_called_once_with(day=1)
 

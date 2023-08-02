8343ab954a4f6918cbb389730b823762853f10cf
Ashwini Sriram <ashwinisriram@Ashwinis-MBP.Excitel>
2023-08-02 20:19:20 +0530

    added todos in test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -24,11 +24,22 @@ class TestService(BaseTestCase):
             call_solution(day)
         mock_fetch_input.assert_called_once_with(day)
 
+    # TODO complete test for get_puzzle_answer with actual input 
+
     def test_call_solution_for_answer(self):
         day = 1
         input = "xyz"
         with patch("solution.get_puzzle_answer") as mock_get_puzzle_answer:
             call_solution(day)
         mock_get_puzzle_answer.assert_called_once
+
+    
+
+    # TODO add test for checking if input file writes only once
+
+
+    def test_get_input_writes_only_once(self):
+        pass
+
 
 

4efa72c87fa93568456e6cce5345e5502f9a9b4d
prachi-goenka <prachigoenka688@gmail.com>
2023-08-03 16:41:22 +0530

    corrected a typo

--- solution.py
+++ solution.py
@@ -1,12 +1,12 @@
-from puzzle import PuzzleSolutionInterfac
+from puzzle import PuzzleSolutionInterface
 
-class YourSolutionForPuzzle1(PuzzleSolutionInterfac):
+class YourSolutionForPuzzle1(PuzzleSolutionInterface):
     def get_puzzle_answer(input):
       #overrides PuzzleSolutionInterface.get_puzzle_answer()
         puzzle_answer = ''
         return puzzle_answer
 
-class YourSolutionForPuzzle2(PuzzleSolutionInterfac):
+class YourSolutionForPuzzle2(PuzzleSolutionInterface):
     def get_puzzle_answer(input):
       #overrides PuzzleSolutionInterface.get_puzzle_answer()
         puzzle_answer = ''
